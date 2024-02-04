fa2bc3cf7fc841ea0926e5c0c191c5d15d7581f3
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-02-04 13:19:27 +0100

    remove dependency

--- src/store/selectors/combination/selectOccupantsToDraw.ts
+++ src/store/selectors/combination/selectOccupantsToDraw.ts
@@ -2,19 +2,19 @@ import { createSelector } from '@reduxjs/toolkit';
 import { checkQuestCondition } from '../../../functions/checkQuestCondition';
 import { isOccupantWithSprite } from '../../../functions/typeguards/isOccupantWithDialogue';
 import { selectOccupants } from '../map/selectOccupants';
-import { selectSaveFile } from '../saveFile/selectSaveFile';
+import { selectQuests } from '../saveFile/selectQuests';
 
 export const selectOccupantsToDraw = createSelector(
-	[selectOccupants, selectSaveFile],
-	(occupants, saveFile) => {
-		if (!saveFile) {
+	[selectOccupants, selectQuests],
+	(occupants, quests) => {
+		if (!quests) {
 			return {};
 		}
 		return Object.fromEntries(
 			Object.entries(occupants).filter(
 				(entry) =>
 					isOccupantWithSprite(entry[1]) &&
-					checkQuestCondition(saveFile.quests, entry[1].questCondition)
+					checkQuestCondition(quests, entry[1].questCondition)
 			)
 		);
 	}
