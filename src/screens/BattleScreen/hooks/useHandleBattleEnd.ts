d5a8ba6beabdb816ee0831d01c0125c141bbc298
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-02-09 21:07:50 +0100

    save on endsBattle

--- src/screens/BattleScreen/hooks/useHandleBattleEnd.ts
+++ src/screens/BattleScreen/hooks/useHandleBattleEnd.ts
@@ -1,5 +1,6 @@
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useSaveGame } from '../../../hooks/useSaveGame';
 import { RoutesEnum } from '../../../router/router';
 import { addDialogue } from '../../../store/slices/dialogueSlice';
 import { useAppDispatch } from '../../../store/storeHooks';
@@ -8,13 +9,15 @@ import { BattleEndReason } from '../interfaces/BattleSnapshot';
 export const useHandleBattleEnd = () => {
 	const navigate = useNavigate();
 	const dispatch = useAppDispatch();
+	const save = useSaveGame();
 	return useCallback(
 		(reason: BattleEndReason) => {
 			if (reason === 'RUN_AWAY') {
 				dispatch(addDialogue(['you escaped the wild Pokemon']));
-				navigate(RoutesEnum.overworld);
 			}
+			save({});
+			navigate(RoutesEnum.overworld);
 		},
-		[dispatch, navigate]
+		[dispatch, navigate, save]
 	);
 };
