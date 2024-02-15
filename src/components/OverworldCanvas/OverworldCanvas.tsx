915846fdb6ed30a6f5e57a2e7dfd180459d9cae6
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-02-15 04:06:05 +0100

    correct z-indexes for overworld

--- src/components/OverworldCanvas/OverworldCanvas.tsx
+++ src/components/OverworldCanvas/OverworldCanvas.tsx
@@ -1,13 +1,13 @@
 import { CSSProperties, useEffect } from 'react';
 import { size } from '../../main';
+import { selectMap } from '../../store/selectors/map/selectMap';
+import { selectPosition } from '../../store/selectors/saveFile/selectPosition';
 import { useAppSelector } from '../../store/storeHooks';
 import './OverworldCanvas.css';
 import { useAnimationFrame } from './hooks/useAnimationFrame';
 import { useDrawBackGroundCanvas } from './hooks/useDrawBackgroundCanvas';
 import { useDrawOccupants } from './hooks/useDrawOccupants';
 import { useDrawPlayerCanvas } from './hooks/useDrawPlayerCanvas';
-import { selectMap } from '../../store/selectors/map/selectMap';
-import { selectPosition } from '../../store/selectors/saveFile/selectPosition';
 
 export const fps = 15;
 export const playerCanvas = 'playerCanvas';
@@ -40,9 +40,22 @@ export const OverworldCanvas = (): JSX.Element => {
 				} as CSSProperties
 			}
 		>
-			<canvas id={playerCanvas} width={size} height={size * 2} />
+			<canvas
+				id={playerCanvas}
+				style={{ zIndex: `calc(var(--PLAYER_CANVAS_INDEX) + ${y} + 1)` }}
+				width={size}
+				height={size * 2}
+			/>
+			{Array.from({ length: height }).map((_, i) => (
+				<canvas
+					id={`occupantCanvas${i}`}
+					className={`occupantCanvas`}
+					style={{ zIndex: `calc(var(--OVERWORLD_CANVAS_INDEX) + ${i} + 1)` }}
+					width={size * width}
+					height={size * height}
+				/>
+			))}
 
-			<canvas id={occupantCanvas} width={size * width} height={size * height} />
 			<canvas
 				id={overworldCanvas}
 				width={size * width}
--- src/components/OverworldCanvas/hooks/useDrawOccupants.ts
+++ src/components/OverworldCanvas/hooks/useDrawOccupants.ts
@@ -15,11 +15,15 @@ export const useDrawOccupants = () => {
 
 	const drawOccupants = useCallback(() => {
 		// console.log('drawOccupants', occupants);
-		const canvas: HTMLCanvasElement | null = document.querySelector(
-			`#${occupantCanvas}`
-		);
-		// Initialize the GL context
-		if (canvas) {
+
+		occupants.forEach((o) => {
+			const canvas: HTMLCanvasElement | null = document.querySelector(
+				`#${occupantCanvas}${o.position.y}`
+			);
+			// Initialize the GL context
+			if (!canvas) {
+				return;
+			}
 			const ctx = canvas.getContext('2d');
 
 			// Only continue if WebGL is available and working
@@ -31,78 +35,96 @@ export const useDrawOccupants = () => {
 			}
 			//clear entire canvas to remove stale
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			occupants.forEach((o) => {
-				const img = new Image();
-				if (!isOccupantWithSprite(o)) {
-					return;
-				}
-				if (o.type === 'ITEM') {
-					img.onload = () => {
-						drawLargeObject({
-							context: ctx,
-							img,
-							x: o.position.x,
-							y: o.position.y,
-							height: 1,
-							width: 1,
-						});
-					};
-					img.src = `mapObjects/pokeball.png`;
-				} else if (o.type === 'LARGE_OBSTACLE') {
-					img.onload = () => {
-						drawLargeObject({
-							context: ctx,
-							img,
-							x: o.position.x,
-							y: o.position.y - (o.height - 1),
-							height: o.height,
-							width: o.width,
-						});
-					};
-					img.src = `mapObjects/${o.sprite}.png`;
-				} else if (o.type === 'OBSTACLE') {
-					img.onload = () => {
-						drawLargeObject({
-							context: ctx,
-							img,
-							x: o.position.x,
-							y: o.position.y,
-							height: 1,
-							width: 1,
-						});
-					};
-					img.src = `mapObjects/${o.sprite}.png`;
-				} else {
-					img.onload = () => {
-						drawCharacter({
-							context: ctx,
-							img,
-							x: o.position.x,
-							y: o.position.y - 0.5,
-							orientation: o.position.orientation,
-							forwardFoot: 0,
-							height: 1.5,
-							width: 1,
-						});
-					};
-					img.src = `npcs/NPC_${o.sprite}.png`;
-				}
-			});
-			decorators.forEach((o) => {
-				const img = new Image();
+
+			const img = new Image();
+			if (!isOccupantWithSprite(o)) {
+				return;
+			}
+			if (o.type === 'ITEM') {
 				img.onload = () => {
 					drawLargeObject({
 						context: ctx,
 						img,
-						x: o.x,
-						y: o.y,
+						x: o.position.x,
+						y: o.position.y,
+						height: 1,
+						width: 1,
+					});
+				};
+				img.src = `mapObjects/pokeball.png`;
+			} else if (o.type === 'LARGE_OBSTACLE') {
+				img.onload = () => {
+					drawLargeObject({
+						context: ctx,
+						img,
+						x: o.position.x,
+						y: o.position.y - (o.height - 1),
+						height: o.height,
+						width: o.width,
+					});
+				};
+				img.src = `mapObjects/${o.sprite}.png`;
+			} else if (o.type === 'OBSTACLE') {
+				img.onload = () => {
+					drawLargeObject({
+						context: ctx,
+						img,
+						x: o.position.x,
+						y: o.position.y,
 						height: 1,
 						width: 1,
 					});
 				};
 				img.src = `mapObjects/${o.sprite}.png`;
-			});
-		}
+			} else {
+				img.onload = () => {
+					drawCharacter({
+						context: ctx,
+						img,
+						x: o.position.x,
+						y: o.position.y - 0.5,
+						orientation: o.position.orientation,
+						forwardFoot: 0,
+						height: 1.5,
+						width: 1,
+					});
+				};
+				img.src = `npcs/NPC_${o.sprite}.png`;
+			}
+		});
+		decorators.forEach((o) => {
+			const canvas: HTMLCanvasElement | null = document.querySelector(
+				`#${occupantCanvas}${o.y}`
+			);
+			// Initialize the GL context
+			if (!canvas) {
+				return;
+			}
+			const ctx = canvas.getContext('2d');
+
+			// Only continue if WebGL is available and working
+			if (ctx === null) {
+				alert(
+					'Unable to initialize 2d context. Your browser or machine may not support it.'
+				);
+				return;
+			}
+			//clear entire canvas to remove stale
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+			const img = new Image();
+			img.onload = () => {
+				drawLargeObject({
+					context: ctx,
+					img,
+					x: o.x,
+					y: o.y,
+					height: 1,
+					width: 1,
+				});
+			};
+			img.src = `mapObjects/${o.sprite}.png`;
+		});
 	}, [decorators, occupants]);
 	return { hasChanges: false, drawOccupants };
 };
