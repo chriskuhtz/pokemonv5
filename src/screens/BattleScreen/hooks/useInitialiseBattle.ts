7a3b7fe591cd7065e4f956c836214d241d7f6392
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-02-16 23:42:11 +0100

    fix battle error

--- src/screens/BattleScreen/hooks/useInitialiseBattle.ts
+++ src/screens/BattleScreen/hooks/useInitialiseBattle.ts
@@ -1,10 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useLazyGetPokemonDataByDexIdQuery } from '../../../api/pokeApi';
 import { useFetch } from '../../../hooks/useFetch';
+import { useGetCurrentSaveFile } from '../../../hooks/xata/useCurrentSaveFile';
 import { BattlePokemon } from '../../../interfaces/BattlePokemon';
-import { selectSaveFile } from '../../../store/selectors/saveFile/selectSaveFile';
-import { useAppSelector } from '../../../store/storeHooks';
 import { BattleSide } from '../BattleScreen';
 import {
 	createBattlePokemonFromData,
@@ -17,13 +16,15 @@ export const useInitialiseBattleSides = (
 	setOpponentSide: React.Dispatch<React.SetStateAction<BattleSide | undefined>>,
 	activePokemonPerSide: number
 ) => {
-	const data = useAppSelector(selectSaveFile);
+	const data = useGetCurrentSaveFile();
 	const { state } = useLocation();
 	const { opponents } = state as BattleScreenProps;
 
 	const [getPokemonByDexId] = useLazyGetPokemonDataByDexIdQuery();
 
-	const { res: allOpponentPokemon } = useFetch<BattlePokemon[]>(() =>
+	const { res: allOpponentPokemon, status: opponentFetchStatus } = useFetch<
+		BattlePokemon[]
+	>(() =>
 		Promise.all(
 			opponents.map(async (e) => {
 				const data = await getPokemonByDexId(e.dexId).unwrap();
@@ -31,25 +32,50 @@ export const useInitialiseBattleSides = (
 			})
 		)
 	);
-	const { res: allPlayerPokemon } = useFetch<BattlePokemon[]>(() =>
-		Promise.all(
-			(data?.pokemon ?? [])
-				.filter((p) => p.onTeam)
-				.map(async (p) => {
-					const data = await getPokemonByDexId(p.dexId).unwrap();
 
-					return createBattlePokemonFromOwned(p, data);
-				})
-		)
-	);
+	const fetchPlayerPokemon = useMemo(() => {
+		if (!data) {
+			return () => Promise.all([]);
+		}
+
+		return () =>
+			Promise.all(
+				data.pokemon
+					.filter((p) => p.onTeam)
+					.map(async (p) => {
+						const data = await getPokemonByDexId(p.dexId).unwrap();
+
+						return createBattlePokemonFromOwned(p, data);
+					})
+			);
+	}, [data, getPokemonByDexId]);
+
+	const { res: allPlayerPokemon, status: playerFetchStatus } = useFetch<
+		BattlePokemon[] | undefined
+	>(fetchPlayerPokemon);
+	useEffect(() => {
+		//initialise battle
+		if (
+			opponentFetchStatus === 'success' &&
+			allOpponentPokemon &&
+			allOpponentPokemon?.length > 0
+		) {
+			setOpponentSide({
+				bench: [],
+				defeated: [],
+				caught: [],
+				side: 'OPPONENT',
+				field: allOpponentPokemon,
+			});
+		}
+	}, [allOpponentPokemon, opponentFetchStatus, setOpponentSide]);
 	useEffect(() => {
 		//initialise battle
 		if (
 			data &&
+			playerFetchStatus === 'success' &&
 			allPlayerPokemon &&
-			allPlayerPokemon?.length > 0 &&
-			allOpponentPokemon &&
-			allOpponentPokemon?.length > 0
+			allPlayerPokemon?.length > 0
 		) {
 			const ablePlayerPokemon = allPlayerPokemon.filter(
 				(p) => p.damage < p.maxHp
@@ -64,21 +90,12 @@ export const useInitialiseBattleSides = (
 				caught: [],
 				side: 'PLAYER',
 			});
-			setOpponentSide({
-				bench: [],
-				defeated: [],
-				caught: [],
-				side: 'OPPONENT',
-				field: allOpponentPokemon,
-			});
 		}
 	}, [
-		data,
-		allOpponentPokemon,
-		allPlayerPokemon,
-		opponents,
-		setPlayerSide,
-		setOpponentSide,
 		activePokemonPerSide,
+		allPlayerPokemon,
+		data,
+		playerFetchStatus,
+		setPlayerSide,
 	]);
 };
