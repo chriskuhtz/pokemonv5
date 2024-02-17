2204972b8330877fac9be17c73e59ad36f0ca237
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-02-17 23:51:11 +0100

    fix test

--- src/screens/TestArea/TestArea.tsx
+++ src/screens/TestArea/TestArea.tsx
@@ -1,19 +1,5 @@
 import { ChooseAction } from '../../components/ChooseActionAndTarget/components/ChooseAction';
-import { BattleAction, BattlePokemon } from '../../interfaces/BattlePokemon';
-
-const pokemon: BattlePokemon = {
-	id: '1cb7406e-2aab-488a-8f86-cff89e71334d',
-	xp: 200,
-	name: 'rattata',
-	side: 'PLAYER',
-	dexId: 19,
-	maxHp: 19,
-	attack: 11,
-	damage: 5,
-	onTeam: true,
-	ownerId: 'opponent',
-	base_experience: 51,
-};
+import { BattleAction } from '../../interfaces/BattlePokemon';
 
 export const TestArea = (): JSX.Element => {
 	return (
