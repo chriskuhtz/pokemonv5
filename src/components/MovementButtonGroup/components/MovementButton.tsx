683ab022e7c46b73ed69b420e4162c272a3628e6
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-02-17 12:04:41 +0100

    nicer movement buttons

--- src/components/MovementButtonGroup/components/MovementButton.tsx
+++ src/components/MovementButtonGroup/components/MovementButton.tsx
@@ -1,3 +1,9 @@
+import {
+	FaCircleArrowDown,
+	FaCircleArrowLeft,
+	FaCircleArrowRight,
+	FaCircleArrowUp,
+} from 'react-icons/fa6';
 import { OrientationEnum } from '../../../interfaces/Orientation';
 import { selectIsWalking } from '../../../store/selectors/saveFile/selectIsWalking';
 import { selectNextOrientation } from '../../../store/selectors/saveFile/selectNextOrientation';
@@ -6,17 +12,99 @@ import {
 	startWalking,
 	stopWalking,
 } from '../../../store/slices/saveFileSlice';
-
 import { useAppDispatch, useAppSelector } from '../../../store/storeHooks';
+import './MovementButton.css';
 
 export const MovementButton = ({ x }: { x: OrientationEnum }): JSX.Element => {
 	const dispatch = useAppDispatch();
 	const nextOrientation = useAppSelector(selectNextOrientation);
 	const isWalking = useAppSelector(selectIsWalking);
 
+	if (x === 1) {
+		return (
+			<FaCircleArrowLeft
+				className="movementButton"
+				key={x}
+				onPointerDown={() => {
+					if (nextOrientation !== x) {
+						dispatch(setNextOrientation(x as OrientationEnum));
+					}
+					if (!isWalking) {
+						dispatch(startWalking());
+					}
+				}}
+				onPointerLeave={() => dispatch(stopWalking())}
+				onMouseLeave={() => dispatch(stopWalking())}
+				onMouseUp={() => dispatch(stopWalking())}
+				onMouseDown={() => {
+					if (nextOrientation !== x) {
+						dispatch(setNextOrientation(x as OrientationEnum));
+					}
+					if (!isWalking) {
+						dispatch(startWalking());
+					}
+				}}
+			/>
+		);
+	}
+	if (x === 2) {
+		return (
+			<FaCircleArrowRight
+				className="movementButton"
+				key={x}
+				onPointerDown={() => {
+					if (nextOrientation !== x) {
+						dispatch(setNextOrientation(x as OrientationEnum));
+					}
+					if (!isWalking) {
+						dispatch(startWalking());
+					}
+				}}
+				onPointerLeave={() => dispatch(stopWalking())}
+				onMouseLeave={() => dispatch(stopWalking())}
+				onMouseUp={() => dispatch(stopWalking())}
+				onMouseDown={() => {
+					if (nextOrientation !== x) {
+						dispatch(setNextOrientation(x as OrientationEnum));
+					}
+					if (!isWalking) {
+						dispatch(startWalking());
+					}
+				}}
+			/>
+		);
+	}
+
+	if (x === 3) {
+		return (
+			<FaCircleArrowUp
+				className="movementButton"
+				key={x}
+				onPointerDown={() => {
+					if (nextOrientation !== x) {
+						dispatch(setNextOrientation(x as OrientationEnum));
+					}
+					if (!isWalking) {
+						dispatch(startWalking());
+					}
+				}}
+				onPointerLeave={() => dispatch(stopWalking())}
+				onMouseLeave={() => dispatch(stopWalking())}
+				onMouseUp={() => dispatch(stopWalking())}
+				onMouseDown={() => {
+					if (nextOrientation !== x) {
+						dispatch(setNextOrientation(x as OrientationEnum));
+					}
+					if (!isWalking) {
+						dispatch(startWalking());
+					}
+				}}
+			/>
+		);
+	}
 	return (
-		<button
-			style={{ userSelect: 'none' }}
+		<FaCircleArrowDown
+			className="movementButton"
 			key={x}
 			onPointerDown={() => {
 				if (nextOrientation !== x) {
@@ -37,8 +125,6 @@ export const MovementButton = ({ x }: { x: OrientationEnum }): JSX.Element => {
 					dispatch(startWalking());
 				}
 			}}
-		>
-			{OrientationEnum[x as OrientationEnum]}
-		</button>
+		/>
 	);
 };
