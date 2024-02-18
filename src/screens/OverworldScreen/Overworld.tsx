1e83de546f6fed54222dccb325d197d1055c7724
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-02-18 12:25:36 +0100

    error if portrait

--- src/screens/OverworldScreen/Overworld.tsx
+++ src/screens/OverworldScreen/Overworld.tsx
@@ -6,10 +6,22 @@ import { RouterButton } from '../../components/RouterButton/RouterButton';
 import { RoutesEnum } from '../../router/router';
 import { selectCurrentDialogue } from '../../store/selectors/dialogue/selectCurrentDialogue';
 import { useAppSelector } from '../../store/storeHooks';
+import { ErrorMessage } from '../../ui_components/ErrorMessage/ErrorMessage';
+
+const isPortrait = ['portrait-secondary', 'portrait-primary'].includes(
+	screen.orientation.type
+);
 
 export const Overworld = (): JSX.Element => {
 	const currentDialogue = useAppSelector(selectCurrentDialogue);
 
+	if (isPortrait) {
+		return (
+			<ErrorMessage
+				message={'Please turn your device sideways and reload the page'}
+			/>
+		);
+	}
 	return (
 		<>
 			{currentDialogue.length === 0 && (
