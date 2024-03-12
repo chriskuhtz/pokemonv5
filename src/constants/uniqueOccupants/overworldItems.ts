67122a316e5335b05d2b1e4e7461bb5fe49a42fc
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-03-12 23:50:36 +0100

    change overworld items

--- src/constants/uniqueOccupants/overworldItems.ts
+++ src/constants/uniqueOccupants/overworldItems.ts
@@ -4,7 +4,7 @@ import { OverworldItem } from '../../screens/OverworldScreen/interfaces/Occupant
 export const starterTownItem1: OverworldItem = {
 	id: 'starter-town-item-1',
 	type: 'ITEM',
-	inventory: generateInventory({ potion: 3 }),
+	inventory: generateInventory({ 'nest-ball': 3 }),
 	position: {
 		y: 7,
 		x: 0,
@@ -15,7 +15,7 @@ export const starterTownItem1: OverworldItem = {
 export const berryPatchItem1: OverworldItem = {
 	id: 'berry-patch-item-1',
 	type: 'ITEM',
-	inventory: generateInventory({ 'great-ball': 3 }),
+	inventory: generateInventory({ 'net-ball': 5 }),
 	position: {
 		y: 0,
 		x: 11,
