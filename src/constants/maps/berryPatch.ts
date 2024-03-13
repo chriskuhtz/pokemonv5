a5355107ce9f304026219e96550296bc895659f8
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-03-13 12:49:29 +0100

    add custom moves to help defeat brock

--- src/constants/maps/berryPatch.ts
+++ src/constants/maps/berryPatch.ts
@@ -8,8 +8,8 @@ export const berryPatchEncounters: MapEncounter[] = [
 	{ xp: 100, dexId: 415, rarity: 2 },
 	{ xp: 100, dexId: 11, rarity: 2 },
 	{ xp: 100, dexId: 14, rarity: 2 },
-	{ xp: 100, dexId: 43, rarity: 2 },
-	{ xp: 200, dexId: 214, rarity: 1 },
+	{ xp: 100, dexId: 43, rarity: 2, customMoves: ['leaf-blade'] },
+	{ xp: 200, dexId: 214, rarity: 1, customMoves: ['cross-chop'] },
 ];
 
 const mapId = 'berry-patch';
