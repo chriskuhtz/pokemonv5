081619d750f828fba75b7e517544fbbe9b941a46
Chris Kuhtz <christopherkuhtz@gmail.com>
2024-03-20 12:41:51 +0100

    disable rotating tags

--- src/components/CircularSprite/components/TagWrapper.tsx
+++ src/components/CircularSprite/components/TagWrapper.tsx
@@ -1,34 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useTags } from '../../../hooks/useTags';
 import { BattlePokemon } from '../../../interfaces/BattlePokemon';
 import '../CircularSprite.css';
-import { useTags } from '../../../hooks/useTags';
 
 export const TagWrapper = ({ pokemon }: { pokemon: BattlePokemon }) => {
 	const tags = useTags(pokemon);
 
-	const [shiftDegrees, setShiftDegrees] = useState<number>(0);
+	//const [shiftDegrees, setShiftDegrees] = useState<number>(0);
+
+	//rotating tags, disabled for now
+	// useEffect(() => {
+	// 	//find the correct angles for n tags
+
+	// 	const rotator = setInterval(() => {
+	// 		setShiftDegrees(shiftDegrees + 0.5);
+	// 		document.querySelectorAll('.tagWrapper').forEach((tagWrapper) => {
+	// 			const circles = tagWrapper.querySelectorAll('.battleSpriteTag');
+	// 			let angle = 360 - 90 + shiftDegrees;
+	// 			const dangle = 360 / circles.length;
+	// 			for (let i = 0; i < circles.length; ++i) {
+	// 				const circle = circles[i] as HTMLElement;
+	// 				angle += dangle;
+	// 				circle.style.transform = `rotate(${angle}deg) translate(${
+	// 					tagWrapper.clientWidth / 2
+	// 				}px) rotate(-${angle}deg)`;
+	// 			}
+	// 		});
+	// 	}, 75);
+
+	// 	return () => clearInterval(rotator);
+	// }, [shiftDegrees, tags]);
 
 	useEffect(() => {
 		//find the correct angles for n tags
 
-		const rotator = setInterval(() => {
-			setShiftDegrees(shiftDegrees + 0.5);
-			document.querySelectorAll('.tagWrapper').forEach((tagWrapper) => {
-				const circles = tagWrapper.querySelectorAll('.battleSpriteTag');
-				let angle = 360 - 90 + shiftDegrees;
-				const dangle = 360 / circles.length;
-				for (let i = 0; i < circles.length; ++i) {
-					const circle = circles[i] as HTMLElement;
-					angle += dangle;
-					circle.style.transform = `rotate(${angle}deg) translate(${
-						tagWrapper.clientWidth / 2
-					}px) rotate(-${angle}deg)`;
-				}
-			});
-		}, 75);
-
-		return () => clearInterval(rotator);
-	}, [shiftDegrees, tags]);
+		document.querySelectorAll('.tagWrapper').forEach((tagWrapper) => {
+			const circles = tagWrapper.querySelectorAll('.battleSpriteTag');
+			let angle = 360 - 90;
+			const dangle = 360 / circles.length;
+			for (let i = 0; i < circles.length; ++i) {
+				const circle = circles[i] as HTMLElement;
+				angle += dangle;
+				circle.style.transform = `rotate(${angle}deg) translate(${
+					tagWrapper.clientWidth / 2
+				}px) rotate(-${angle}deg)`;
+			}
+		});
+	}, [tags]);
 
 	return (
 		<div className="tagWrapper">
